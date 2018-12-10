25b8cfd6fca08208cf4e92b135781c4ec31643bb
Niarfe <efrain.olivares@gmail.com>
2018-12-09 23:17:48 -0800

    fix up tests for automata dfs

--- hydraseq/automata.py
+++ hydraseq/automata.py
@@ -1,23 +1,26 @@
+"""
+Deterministic Finite Automata
+Examples taken from Understanding Computation Chapter 2
+
+Conversion to mermaid cretes directly graphable in html using mermaidjs
+"""
 import hydraseq as hd
 
-def convert_to_mermaid(lst_transitions):
-    """Takes a 'st1 a st2' list of transitions and generates a mermaid compatible
-        string like 'st1((st1)) --a--> st2((st2))' """
-    def convert_line(str_line):
-        start, action, final = str_line.split()
-        final = final.replace('^', '')
-        return "{}(({})) --{}--> {}(({}))".format(start, start, action, final, final)
-    return "\n".join([convert_line(line) for line in lst_transitions])
-
 class DFAstate:
-    def __init__(self, transitions, init_state):
+    def __init__(self, transitions, init_state, accepting_states=[]):
         self.states = hd.Hydraseq('_')
         self.init_state = init_state
+        self.acceptings = accepting_states
+        self.transitions = transitions
         [self.states.insert(transition) for transition in transitions]
         self.reset()
 
     def get_active_states(self):
         return self.states.get_active_values()
+
+    def in_accepting(self):
+        """Return True if current state is one of accepting states"""
+        return any([state in self.acceptings for state in self.states.get_active_values()])
 
     def reset(self, init_state=None):
         self.states.look_ahead(init_state if init_state else self.init_state)
@@ -29,20 +32,20 @@ class DFAstate:
             self.states.look_ahead(action.replace('^', ''))
         return self
 
+    def read_string(self, str):
+        self.reset()
+        [self.event(char) for char in str]
+        return self
+
+    def convert_to_mermaid(self):
+        """Takes a 'st1 a st2' list of transitions and generates a mermaid compatible
+            string like 'st1((st1)) --a--> st2((st2))' """
+        def _convert_line(str_line):
+            start, action, final = str_line.split()
+            final = final.replace('^', '')
+            return "{}(({})) --{}--> {}(({}))".format(start, start, action, final, final)
+
+        return "\n".join([_convert_line(line) for line in self.transitions])
+
     def __str__(self):
         return "DFA state: {}, preds: {}".format(self.states.get_active_values(), self.states.get_next_values())
-
-
-if __name__ == "__main__":
-    transitions = [
-        "s1 a ^s2",
-        "s1 b ^s1",
-        "s2 a ^s2",
-        "s2 b ^s3",
-        "s3 a ^s3",
-        "s3 b ^s3"
-    ]
-    dfa = DFAstate(transitions, 's1')
-    print(dfa)
-    for letter in "bbabba":
-        print(letter, dfa.event(letter))
--- tests/test_automata.py
+++ tests/test_automata.py
@@ -24,23 +24,45 @@ s2((s2)) --a--> s2((s2))
 s2((s2)) --b--> s3((s3))
 s3((s3)) --a--> s3((s3))
 s3((s3)) --b--> s3((s3))'''
-    assert convert_to_mermaid(transitions) == expected
+    dfa = DFAstate(transitions, 's1')
+    assert dfa.convert_to_mermaid() == expected
 
-
+#
+# Examples are from understanding compuation chapter 2
+#
 def test_two_state():
+    """Simple toggle between two states"""
     transitions = [
         "s1 a ^s2",
         "s2 a ^s1"
     ]
     dfa = DFAstate(transitions, 's1')
+
     input_string = "aaaaaa"
     current_state = 's1'
     for letter in input_string:
         current_state = 's1' if current_state == 's2' else 's2'
         assert current_state == dfa.event(letter).get_active_states()[0]
 
-def test_tri_state_automata():
+def test_two_state_accept_odds():
+    """Toggle between two states, but accepting only on s2.  Accepts odd length strings"""
+    transitions = [
+        "s1 a ^s2",
+        "s2 a ^s1"
+    ]
+    dfa = DFAstate(transitions, 's1', accepting_states=['s2'])
 
+    input_string = "aaaaaa"
+    current_state = 's1'
+    accepting = False
+    for letter in input_string:
+        current_state = 's1' if current_state == 's2' else 's2'
+        accepting = False if accepting else True
+        assert current_state == dfa.event(letter).get_active_states()[0]
+        assert accepting == dfa.in_accepting()
+
+def test_tri_state_automata():
+    """Only accept strings that contain sequence ab. Pg 65"""
     transitions = [
         "s1 a ^s2",
         "s1 b ^s1",
@@ -49,7 +71,20 @@ def test_tri_state_automata():
         "s3 a ^s3",
         "s3 b ^s3"
     ]
-    dfa = DFAstate(transitions, 's1')
-    print(dfa)
-    for letter in "bbabba":
-        print(letter, dfa.event(letter))
+    dfa = DFAstate(transitions, 's1', accepting_states=['s3'])
+    print(dfa.convert_to_mermaid())
+
+    accepts = [
+        "abaa",
+        "aaaaaba",
+        "aaaaaaab"
+    ]
+    rejects = [
+        "aaaaa",
+        "aaaaaaa",
+        "baaaa"
+    ]
+    for accept in accepts:
+        assert dfa.read_string(accept).in_accepting() == True, accept + " should be accepted"
+    for reject in rejects:
+        assert dfa.read_string(reject).in_accepting() == False, reject+" should be rejected"
