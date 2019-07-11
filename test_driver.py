c2c20d9e7e3b793c86501d04e4bd86ca49738361
EfrainOlivaresCD <efrain.olivares@rivdata.com>
2019-07-10 22:05:28 -0700

    add efrain to test driver

--- test_driver.py
+++ test_driver.py
@@ -1,8 +1,22 @@
 from hydraseq import MiniColumn
 
-source_files = ['linear.0.000', 'linear.1.001', 'linear.2.002', 'linear.3.003']
+source_files = [
+        'linear.0.000',
+        'linear.1.001',
+        'linear.2.002',
+        'linear.3.003'
+        ]
 data_dir = 'tests/data/'
 
 mcol = MiniColumn(source_files, data_dir)
 
-ctree = mcol.compute_convolution_tree(". - .")
+#ctree = mcol.compute_convolution_tree(". - .")
+
+efrain = ". . . - . . - . . - . . - ."
+
+ctree = mcol.compute_convolution_tree(efrain)
+
+for hydra in mcol.hydras:
+    print(hydra.active_nodes)
+
+
