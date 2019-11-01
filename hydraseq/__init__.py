83d394ca74861159cc1009c76da2116287e16867
EfrainOlivaresCD <efrain.olivares@rivdata.com>
2019-10-31 22:33:57 -0700

    move minicolumn to own repo

--- hydraseq/__init__.py
+++ hydraseq/__init__.py
@@ -2,5 +2,4 @@ name = "hydraseq"
 __version__ = '0.0.27'
 from hydraseq.hydraseq import Node
 from hydraseq.hydraseq import Hydraseq
-from hydraseq.minicolumn import MiniColumn
 from hydraseq.automata import DFAstate
